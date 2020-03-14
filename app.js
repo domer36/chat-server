58953e09f7aee7fc72541ddf99aa8c23fc495df7
Carlos Dominguez <carlos.cfdr86@gmail.com>
2020-03-14 01:43:18 -0600

    CORS supports for API

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const http = require('http')
 const socketIO = require('socket.io')
 
 const app = express()
+
+app.use(
+    cors({
+      credentials: true,
+      origin: [process.env.FRONTENDPOINT]
+    })
+)
+
 const server = http.Server(app)
 const io = socketIO(server);
 
 const PORT = process.env.PORT || 3000;
+
 
 
 const users = []
@@ -34,5 +44,7 @@ io.on('connection', (socket) => {
         io.emit('users', users)
     })
 })
+
+app.use('/getTotalUsers', (_, res) => res.status(200).json({total: users.length}))
 
 server.listen(PORT, () => console.log(`started on port: ${PORT}`))
